2b4d04257618ee3d02c7e57f1bbf4119cb44f0b8
xiyuwuhen <15201732653@163.com>
2016-10-05 15:40:31 +0800

    初始化代码仓库

---
+++ RichText/RichText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RichText
+//
+//  Created by Tesiro on 16/10/5.
+//  Copyright © 2016年 Tesiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RichText/RichText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RichText
+//
+//  Created by Tesiro on 16/10/5.
+//  Copyright © 2016年 Tesiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
